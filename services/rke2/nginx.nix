450f84a8ce8284d1600707cfc6c6906a82740e51
azey <me@azey.net>
2025-08-02 03:29:10 +0200

    services/rke2/nginx.nix: im thinking miku miku ooo eeee ooo

--- services/rke2/nginx.nix
+++ services/rke2/nginx.nix
@@ -12,7 +12,10 @@ with lib; let
 in {
   options.az.svc.rke2.nginx = with azLib.opt; {
     enable = optBool false;
-    repo = optStr "https://git.${domain}/infra/${domain}";
+    repos = {
+      root = optStr "https://git.${domain}/infra/${domain}"; # TODO: https://ogp.me, also put this TODO in the site's repo
+      miku = optStr "https://git.${domain}/mirrors/ooo.eeeee.ooo"; # im thinking miku miku oo eee oo
+    };
   };
 
   config = mkIf cfg.enable {
@@ -35,22 +38,24 @@ in {
           	listen 8080 default_server;
           	listen [::]:8080 default_server;
 
-          	root /srv/current/static;
+          	root /srv/root/current/static;
 
           	index ________________none;
-
           	rewrite ^(?<path>.*)/__autoindex\.json$	$path/			last;
           	rewrite ^(?<path>.*)/$			$path/index.html	last;
 
           	autoindex on;
           	autoindex_exact_size on;
           	autoindex_format json;
+          }
 
-          	location / {
-          		limit_except GET HEAD OPTIONS {
-          			deny all;
-          		}
-          	}
+          server {
+          	listen 8080;
+          	listen [::]:8080;
+          	server_name miku.${domain};
+
+          	root /srv/miku/current;
+          	index index.html;
           }
         '';
       }
@@ -73,32 +78,31 @@ in {
             fsGroup = 65532;
           };
 
-          template.spec.containers = [
-            {
-              name = "git-sync";
+          template.spec.containers = let
+            syncContainer = dir: repo: {
+              name = "git-sync-${dir}";
               image = "registry.k8s.io/git-sync/git-sync:v4.4.2"; # TODO: is there any way to use latest?
               args = [
-                "--repo=${cfg.repo}"
+                "--repo=${repo}"
                 "--depth=1"
                 "--period=300s"
                 "--link=current"
-                "--root=/srv"
+                "--root=/srv/${dir}"
               ];
               volumeMounts = [
                 {
                   name = "nginx-srv";
                   mountPath = "/srv";
                 }
-                {
-                  name = "nginx-cm";
-                  mountPath = "/etc/nginx/conf.d";
-                }
               ];
               securityContext = {
                 allowPrivilegeEscalation = false;
                 capabilities.drop = ["ALL"];
               };
-            }
+            };
+          in [
+            (syncContainer "root" cfg.repos.root)
+            (syncContainer "miku" cfg.repos.miku)
             {
               name = "nginx";
               image = "nginxinc/nginx-unprivileged";
@@ -167,7 +171,7 @@ in {
       {
         name = "nginx";
         namespace = "app-nginx";
-        hostnames = ["${config.az.server.rke2.baseDomain}"];
+        hostnames = ["${domain}"];
         rules = [
           {
             backendRefs = [
@@ -186,11 +190,27 @@ in {
         };
         responseHeaders.x-robots-tag = "all";
       }
+      {
+        name = "nginx-miku";
+        namespace = "app-nginx";
+        hostnames = ["miku.${domain}"];
+        rules = [
+          {
+            backendRefs = [
+              {
+                name = "nginx";
+                port = 8080;
+              }
+            ];
+          }
+        ];
+        csp = "lax";
+      }
     ];
 
     az.svc.rke2.authelia.rules = [
       {
-        domain = ["${domain}"];
+        domain = ["miku.${domain}" "${domain}"];
         methods = ["GET" "HEAD" "OPTIONS"];
         policy = "bypass";
       }
