da7abc5e80a1d763c23a06626673d3e9c8d92104
azey <me@azey.net>
2025-08-13 19:28:12 +0200

    services/rke2/renovate.nix: add automatic upgrades

--- services/rke2/renovate.nix
+++ services/rke2/renovate.nix
@@ -11,14 +11,28 @@ with lib; let
 in {
   options.az.svc.rke2.renovate = with azLib.opt; {
     enable = optBool false;
+    schedule = optStr "0 */2 * * *"; # bi-hourly
+
+    autoUpgrade = {
+      # TODO: factor out as core module, merge with VPS
+      enable = optBool true;
+      flake = optStr "git+https://git.${domain}/infra/nixos-servers";
+    };
   };
 
   config = mkIf cfg.enable {
-    /*
-    az.svc.cron.jobs = [
-      "30 5 * * *  fish -c 'for f in /var/lib/rancher/rke2/server/manifests/*'; kubectl apply -f $f; end"
+    system.autoUpgrade = mkIf cfg.autoUpgrade.enable {
+      enable = true;
+      randomizedDelaySec = "45min";
+      persistent = false;
+      flake = cfg.autoUpgrade.flake;
+      allowReboot = false; # TODO: change w/ multiple nodes
+    };
+    az.svc.cron.enable = lib.mkDefault cfg.autoUpgrade.enable;
+    az.svc.cron.jobs = lib.lists.optionals cfg.autoUpgrade.enable [
+      "30 5 * * *  root  fish -c 'for f in /var/lib/rancher/rke2/server/manifests/*'; kubectl apply -f $f; end"
     ];
-    */
+
     az.server.rke2.manifests."app-renovate" = [
       {
         apiVersion = "v1";
@@ -65,7 +79,7 @@ in {
               seccompProfile.type = "RuntimeDefault";
             };
 
-            cronjob.schedule = "0 4 * * *"; # 4AM daily
+            cronjob.schedule = cfg.schedule;
 
             existingSecret = "renovate-env";
             #envFrom = [{secretRef.name = "renovate-env";}];
