09adda9cb66a4d04e6c4bf4de51d54834310dd62
azey <me@azey.net>
2025-10-14 00:34:46 +0200

    services/rke2/git/woodpecker.nix: add images for steps

--- services/rke2/git/woodpecker.nix
+++ services/rke2/git/woodpecker.nix
@@ -18,8 +18,6 @@ in {
     az.server.rke2.namespaces."app-woodpecker".networkPolicy = {
       fromNamespaces = ["envoy-gateway"];
       toDomains = ["git.${domain}"];
-
-      toWAN = true; # downloads remote images - TODO: local registry
       extraEgress = [{toEntities = ["kube-apiserver"];}];
     };
     az.server.rke2.namespaces."app-forgejo".networkPolicy.toDomains = ["woodpecker.${domain}"]; # push events
@@ -30,6 +28,27 @@ in {
       networkPolicy.toWAN = true; # nix flake updates
     };
 
+    az.server.rke2.images = {
+      # pipeline images
+      woodpecker-ci-plugin-git = {
+        imageName = "woodpeckerci/plugin-git";
+        finalImageTag = "2.7.0";
+        imageDigest = "sha256:1ce87890a4996596c0f6c28f69afe5e07bf106c294f49fb2cfe5971b4a04f70c";
+        hash = "sha256-mB2kjbvIQZFb/FvbySaQ1bs4Zd30MSGJBDdqlugbFcY="; # renovate: woodpeckerci/plugin-git 2.7.0
+      };
+      nixos-nix = {
+        imageName = "nixos/nix";
+        finalImageTag = "2.32.0";
+        imageDigest = "sha256:a3b5d472ca187c25c87a217bc730cdf7a3df7d07ee09eb37f5bc8c874f173a52";
+        hash = "sha256-RXZj3q+VSEFDhdahJGYMzgOUXxk6zDgNB7NxqoWQe5g="; # renovate: nixos/nix 2.32.0
+      };
+      appleboy-drone-git-push = {
+        imageName = "appleboy/drone-git-push";
+        finalImageTag = "1.1.1";
+        imageDigest = "sha256:1c998f23bbbce2ed57ca75c2cebc695eee203d2e18eacf91a5ff64068337a6d9";
+        hash = "sha256-G4pHxMGpVwnwxXlGEPTSMNF0GjWR3Nb/D4a2/4hwe0M="; # renovate: appleboy/drone-git-push 1.1.1
+      };
+    };
     services.rke2.autoDeployCharts."woodpecker" = {
       repo = "oci://ghcr.io/woodpecker-ci/helm/woodpecker";
       version = "3.4.0";
